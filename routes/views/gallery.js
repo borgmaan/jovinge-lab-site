9026ba64dc080216f412bfba45d3b7f65038dc22
Andrew Borgman <ndrwsbrgmn@gmail.com>
2015-04-15 11:10:47 -0400

    test

--- routes/views/gallery.js
+++ routes/views/gallery.js
@@ -1,3 +1,4 @@
+
 var keystone = require('keystone');
 
 exports = module.exports = function(req, res) {
@@ -12,3 +13,40 @@ exports = module.exports = function(req, res) {
 	view.render('gallery');
 	
 }
+
+// var keystone = require('keystone');//,
+// 	// Gallery = keystone.list('Gallery');
+
+// exports = module.exports = function(req, res) {
+	
+// 	var view = new keystone.View(req, res),
+// 		locals = res.locals;
+
+// 	// Set locals
+// 	locals.section = 'gallery';
+// 	// locals.data = {
+// 	// 	galleries: [] // storing all info for our galleries in an array...
+// 	// };
+	
+
+// 	// one way to set this up
+// 	// Gallery.model.find()
+// 	//     .sort('sortOrder')
+// 	//     .exec(function(err, gals) {
+//  //        // do something with posts
+//  //        	console.log('printing query results');
+//  //        	console.log(gals);
+//  //        	locals.data.galleries = gals
+//  //        	if (err) return handleError(err);
+//  //    	});
+
+// // ._.publishedDate.format('Do MMM YYYY')
+
+// 	// this is our current working one...
+// 	view.query('galleries', keystone.list('Gallery').model.find().sort('sortOrder'));
+
+
+	
+// 	view.render('gallery');
+	
+// }
