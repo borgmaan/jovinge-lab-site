e13cf7aeea95606bb49223664b6acd4d202f9a15
Andrew Borgman <ndrwsbrgmn@gmail.com>
2015-04-15 15:23:06 -0400

    ugh, figure out you dont have to put # before the handlebars helpers...

--- routes/views/gallery.js
+++ routes/views/gallery.js
@@ -1,6 +1,10 @@
 
 var keystone = require('keystone');
 
+// var keystone = require('keystone'),
+// 	Gallery = keystone.list('Gallery');
+
+
 exports = module.exports = function(req, res) {
 	
 	var view = new keystone.View(req, res),
@@ -9,44 +13,8 @@ exports = module.exports = function(req, res) {
 	locals.section = 'gallery';
 	
 	view.query('galleries', keystone.list('Gallery').model.find().sort('sortOrder'));
-	
+
 	view.render('gallery');
 	
 }
 
-// var keystone = require('keystone');//,
-// 	// Gallery = keystone.list('Gallery');
-
-// exports = module.exports = function(req, res) {
-	
-// 	var view = new keystone.View(req, res),
-// 		locals = res.locals;
-
-// 	// Set locals
-// 	locals.section = 'gallery';
-// 	// locals.data = {
-// 	// 	galleries: [] // storing all info for our galleries in an array...
-// 	// };
-	
-
-// 	// one way to set this up
-// 	// Gallery.model.find()
-// 	//     .sort('sortOrder')
-// 	//     .exec(function(err, gals) {
-//  //        // do something with posts
-//  //        	console.log('printing query results');
-//  //        	console.log(gals);
-//  //        	locals.data.galleries = gals
-//  //        	if (err) return handleError(err);
-//  //    	});
-
-// // ._.publishedDate.format('Do MMM YYYY')
-
-// 	// this is our current working one...
-// 	view.query('galleries', keystone.list('Gallery').model.find().sort('sortOrder'));
-
-
-	
-// 	view.render('gallery');
-	
-// }
