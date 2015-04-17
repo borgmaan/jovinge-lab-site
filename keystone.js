809e072a5cf46056212d9e586fbadd6dbf2d8379
Andrew Borgman <ndrwsbrgmn@gmail.com>
2015-04-17 09:49:47 -0400

    remove deploy script and stop being dumb

--- keystone.js
+++ keystone.js
@@ -64,6 +64,7 @@ keystone.set('routes', require('./routes'));
 keystone.set('nav', {
 	'posts': ['posts', 'post-categories'],
 	'enquiries': 'enquiries',
+	'enquiries': 'enquiries',
 	'users': 'users'
 });
 
