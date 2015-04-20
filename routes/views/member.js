cd87c49bb9f3532201a103af23e063326c908d1f
Andrew Borgman <ndrwsbrgmn@gmail.com>
2015-04-20 10:52:08 -0400

    manually enforce https on cloudinary images b/c keystone setting is not working right

--- routes/views/member.js
+++ routes/views/member.js
@@ -8,6 +8,7 @@ exports = module.exports = function(req, res) {
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
 	locals.section = 'team'; // this has to match key in /routes/views/middlewear.js
+	// view.query('members', keystone.list('Member').model.find().sort('scientist'));
 	view.query('members', keystone.list('Member').model.find());	
 
 	// Render the view
